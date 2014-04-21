8cd200dbbaffd6fdce043431e23d56e7be4d297a
Thiago Vilarinho <thiago_gomes@msn.com>
2014-04-21 16:25:35 -0300

    Troquei a forma de como o projetil eh destruido

--- ProjectileScript.cs
+++ ProjectileScript.cs
@@ -3,17 +3,18 @@ using System.Collections;
 
 public class ProjectileScript : MonoBehaviour {
 	private Transform playerReferenceTag;
+	public int timeToDestroy = 3;
 	// Use this for initialization
 	void Start () {
-		
+		Destroy(this.gameObject,timeToDestroy);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		playerReferenceTag = GameObject.FindWithTag("Player").transform;
-		if(Vector3.Distance(this.transform.position, playerReferenceTag.position) >15) {
-			Destroy(this.gameObject);
-		}
+		//playerReferenceTag = GameObject.FindWithTag("Player").transform;
+		//if(Vector3.Distance(this.transform.position, playerReferenceTag.position) >15) {
+
+		//}
 
 
 	}
